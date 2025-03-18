d500fc7f9b79f13c11d8306972e1ec21ac5f5303
Kima93 <154838127+Kima93@users.noreply.github.com>
2025-03-18 21:08:01 +0000

    cambios2

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -7,39 +7,19 @@ const formatTime = (seconds) => {
   return `${hrs}:${mins}:${secs}`;
 };
 
-const SecondsCounter = ({ seconds }) => {
-  const timeString = formatTime(seconds);
-
-  return (
-    <div style={{ 
-      fontSize: "2rem", 
-      textAlign: "center", 
-      marginTop: "20px", 
-      display: "flex", 
-      justifyContent: "center", 
-      alignItems: "center", 
-      gap: "10px", 
-      backgroundColor: "black", 
-      padding: "10px", 
-      borderRadius: "10px", 
-      color: "white"
-    }}>
-      <i className="fa-solid fa-clock" style={{ fontSize: "2rem" }}></i>
-      {timeString.split("").map((char, index) => (
-        <span key={index} style={{
-          backgroundColor: "black",
-          color: "white",
-          padding: "10px",
-          fontSize: "2rem",
-          borderRadius: "5px",
-          minWidth: "40px",
-          textAlign: "center"
-        }}>
-          {char}
-        </span>
-      ))}
-    </div>
-  );
-};
+class SecondsCounter extends React.Component {
+  render() {
+    return (
+      <div style={{ fontSize: '40px', textAlign: 'center', marginTop: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', backgroundColor: 'black', padding: '10px', borderRadius: '10px', color: 'white' }}>
+        <i className="fa-solid fa-clock"></i>
+        {this.props.seconds.toString().padStart(6, '0').split('').map((digit, index) => (
+          <div key={index} style={{ backgroundColor: 'black', color: 'white', padding: '10px 15px', borderRadius: '5px', fontSize: '40px' }}>
+            {digit}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default SecondsCounter;
